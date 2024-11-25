883c41d5dcc34e3aa77ae8c57395ed3724a10eb9
thomas.calabrese <thomascalabrese1@freenet.de>
2024-11-25 16:52:13 +0100

    first draft

--- src/App.js
+++ src/App.js
@@ -1,25 +1,317 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  DEGREES,
+  TRAININGS,
+  TOOLS,
+  SKILLS,
+  TECHNOLOGIES,
+  HOBBIES,
+  SHOP_ITEMS,
+  PMT_ITEMS,
+  WORK_EXPERIENCES,
+} from "./texts";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 function App() {
+  var counter = 0;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      {/* <header className="header">Thomas' CV</header> */}
+      <Hero idx={counter++} />
+      <Section title="Ausbildung" idx={counter++}>
+        {DEGREES.map((degree, idx) => (
+          <>
+            <SubSectionPair
+              idx={idx}
+              isLast={idx === DEGREES.length - 1}
+              article={
+                <Article
+                  title={degree.title}
+                  dateStart={degree.dateStart}
+                  dateEnd={degree.dateEnd}
+                  paragraphs={degree.paragraphs}
+                />
+              }
+              image={
+                <img
+                  className="article-img"
+                  src={degree.imgUrl}
+                  title={degree.imgTitle}
+                  alt={degree.imgAlt}
+                />
+              }
+            />
+          </>
+        ))}
+      </Section>
+      <Section title="Arbeitserfahrung" idx={counter++}>
+        {WORK_EXPERIENCES.map((workExperience, idx) => (
+          <>
+            <SubSectionPair
+              idx={idx}
+              article={
+                <Article
+                  title={workExperience.title}
+                  dateStart={workExperience.dateStart}
+                  dateEnd={workExperience.dateEnd}
+                  paragraphs={workExperience.paragraphs}
+                />
+              }
+              image={
+                <img
+                  className="article-img downscale"
+                  src={workExperience.imgUrl}
+                  title={workExperience.imgTitle}
+                  alt={workExperience.imgAlt}
+                />
+              }
+            />
+          </>
+        ))}
+      </Section>
+      <Section title="Fortbildungen" idx={counter++}>
+        <SubSectionGrid articles={TRAININGS} />
+      </Section>
+      <Section title="Arbeitsproben App & Full-Stack" idx={counter++}>
+        <SubSectionPair
+          article={
+            <Article
+              title="Flutter Shop App"
+              dateStart="Juni 2024"
+              paragraphs={[
+                'Die Flutter Shop App enstand als Teil der Udemy Fortbildung "Lerne Flutter & Dart und erstelle native iOS & Android Apps"',
+                "Sie ermöglicht es einen Nutzer zu registrieren, verfügbare Produkte anzuzeigen, eigene Produkte dem Markplatz verfügbar zu machen, Bestellungen zu tätigen, Produkte zu einer Merkliste hinzuzufügen und getätigte Bestellungen anzuzeigen.",
+                "Die App selbst wurde mit Flutter in Dart programmiert. Für die Benutzer-Authentifizierung wird Googles Identity Toolkit API genutzt. Die Shop Daten werden in einer Google Firebase NoSQL Datenbank gespeichert.",
+              ]}
+            />
+          }
+          image={
+            <Carousel
+              className="carousel-flutter"
+              showDots={true}
+              slidesToSlide={1}
+              responsive={{
+                desktop: {
+                  breakpoint: { max: 40000, min: 0 },
+                  items: 1,
+                },
+              }}
+            >
+              {SHOP_ITEMS.map((shop_item) => (
+                <img
+                  key={shop_item.imgUrl}
+                  src={shop_item.imgUrl}
+                  alt={shop_item.alt}
+                  title={shop_item.title}
+                />
+              ))}
+            </Carousel>
+          }
+        />
+        <SubSectionPair
+          idx={1}
+          article={
+            <Article
+              title="React Project Management Tool"
+              dateStart="Oktober 2024"
+              dateEnd="jetzt"
+              paragraphs={[
+                'Das Project Mangement Tool (PMT) entsteht als Lern-Projekt, motiviert durch die Udemy Fortbildung "Master modern React from beginner to advanced".',
+                "PMT ermöglicht es einem Nutzer Projekte, Meilensteine und Aufgaben zu verwalten. Die Funktionalität ist in vier Hauptbereiche aufgeteilt: Aktive Sprints, Sprint Planer, Aufgabenbrowser und Projektübersicht. Übergeordnet ist es jederzeit möglich neue Projekte, Meilensteine oder Aufgaben zu erstellen.",
+                "In Aktive Sprints kann der Nutzer zwischen auf aktiv gesetzten Sprints auswählen und Aufgabenfortschritte verwalten.",
+                "Der Sprint Planer erlaubt es Sprints zu erstellen, Aufgaben den ausgewählten Sprints zuzuweisen und den Status des Sprints selbst von Offen über Aktiv auf Geschlossen zu setzen.",
+                "Unabhängig vom Projekt-/Meilenstein-/Aufgabenzustands kann der Nutzer im Aufgabenbrowser die mit einem Projekt assoziierten Meilensteine und Aufgaben anzuzeigen, modifizieren oder löschen.",
+                "Die Projektübersicht ermöglicht es Meilensteine und Aufgaben mehrerer Projekte übereinander entlang eines Zeitstrahls anzuzeigen.",
+                "PMT ist eine Single Page Application, wird in JavaScript mit der React Library programmiert und mit einem node.js Server lokal betrieben. Aktuell gehen die Daten mit dem Verlassen der Sitzung verloren.",
+              ]}
+            />
+          }
+          image={
+            <div className="mint-logo">
+              <img
+                src="./images/logoMint64.png"
+                title="Logo Project Management Tool"
+                alt="Minzblatt"
+                style={{
+                  backgroundColor: "#006F3D",
+                }}
+              />
+              <p style={{ textAlign: "center", fontSize: "2rem" }}>Mint PMT</p>
+            </div>
+          }
         >
-          Learn React
-        </a>
-      </header>
+          <Carousel
+            className="carousel-pmt"
+            showDots={true}
+            slidesToSlide={1}
+            responsive={{
+              desktop: {
+                breakpoint: { max: 40000, min: 0 },
+                items: 1,
+              },
+            }}
+          >
+            {PMT_ITEMS.map((pmt_item) => (
+              <img
+                key={pmt_item.imgUrl}
+                src={pmt_item.imgUrl}
+                alt={pmt_item.alt}
+                title={pmt_item.title}
+              />
+            ))}
+          </Carousel>
+        </SubSectionPair>
+      </Section>
+      <Section title="Fähigkeiten" idx={counter++}>
+        <SubSectionFeatures features={SKILLS} />
+      </Section>
+      <Section title="Technologien" idx={counter++}>
+        <SubSectionFeatures features={TECHNOLOGIES} />
+      </Section>
+      <Section title="Werkzeuge" idx={counter++}>
+        <SubSectionFeatures features={TOOLS} />
+      </Section>
+      <Section title="Interessen" idx={counter++}>
+        <SubSectionFeatures features={HOBBIES} />
+      </Section>
+      <footer className="footer"></footer>
     </div>
   );
 }
 
 export default App;
+
+function Hero({ idx }) {
+  var isOdd = idx % 2;
+  return (
+    <section className={`${isOdd && "odd"}`}>
+      <div className="px-4 py-5 text-center">
+        <img
+          id="thomas-avatar"
+          className="d-block mx-auto mb-5"
+          src="./images/thomas.jpg"
+          alt="Thomas"
+          width="180"
+          height="180"
+        />
+        <h2 className="display-5 fw-bold">Hi.</h2>
+        <div className="col-lg-6 mx-auto">
+          <p className="lead">
+            Ich bin Thomas, ein professioneller Software Engineer. Nach vielen
+            spannenden Projekten und Jahren im Embedded Bereich hatte ich Lust
+            auf etwas neues und habe mich in App- und Full-Stack-Entwicklung
+            fortgebildet. Abseits von Bits und Bytes interessiere ich mich für
+            Musik und Tanz.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+function Section({ title, idx, className, children }) {
+  var isOdd = idx % 2;
+  return (
+    <section className={`${isOdd && "odd"} ${className}`}>
+      <div className="container col-xxl-8 px-4">
+        <div className="row flex-lg-row py-3">
+          <h2>{title}</h2>
+          {children}
+        </div>
+      </div>
+    </section>
+  );
+}
+
+function SubSectionPair({ article, image, idx, isLast, children }) {
+  const isOdd = idx % 2 === 1;
+  return (
+    <div className={`sub-section row flex-lg-row ${isLast && "last"}`}>
+      <div className="col-10 col-sm-8 col-lg-6 justify-center">
+        {isOdd ? image : article}
+      </div>
+      <div className="col-10 col-sm-8 col-lg-6 justify-center">
+        {isOdd ? article : image}
+      </div>
+      {children && <div className="child">{children}</div>}
+    </div>
+  );
+}
+
+function SubSectionGrid({ articles }) {
+  return (
+    <div className={`sub-section row flex-lg-row`}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+        }}
+      >
+        {articles.map((article, idx) => (
+          <div className="col-10 col-sm-8 col-lg-6 justify-center">
+            <Article
+              key={article.title}
+              className="tile"
+              title={article.title}
+              dateStart={article.dateStart}
+              dateEnd={article.dateEnd}
+              paragraphs={article.paragraphs}
+              idx={idx}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function Article({ className, title, dateStart, dateEnd, paragraphs, idx }) {
+  const isPrimary = idx !== undefined && idx % 3 === 0;
+  const isSecondary = idx !== undefined && idx % 3 === 1;
+  const isTertiary = idx !== undefined && idx % 3 === 2;
+  return (
+    <div
+      className={`article ${className} ${isPrimary && "primary"} ${
+        isSecondary && "secondary"
+      } ${isTertiary && "tertiary"}`}
+    >
+      <h3 className="article-header">{title}</h3>
+      <p className="article-time">
+        {dateStart}
+        {dateEnd !== undefined && ` - ${dateEnd}`}
+      </p>
+      {paragraphs.map((paragraph) => (
+        <p className="article-text">{paragraph}</p>
+      ))}
+    </div>
+  );
+}
+
+function SubSectionFeatures({ features }) {
+  return (
+    <div className={`sub-section row flex-lg-row`}>
+      <div className="container px-4 pt-5">
+        <ul className="row g-4 py-5 row-cols-1 row-cols-lg-3">
+          {features.map((feature) => (
+            <Feature
+              icon={feature.icon}
+              title={feature.title}
+              description={feature.description}
+            />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+function Feature({ title, description, icon }) {
+  return (
+    <li className="feature col">
+      <div>{icon}</div>
+      <h3 className="fs-2">{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
---
+++ src/texts.js
@@ -0,0 +1,300 @@
+import {
+  IconSvgBackback,
+  IconSvgBatteryCharging,
+  IconSvgBook,
+  IconSvgBoomBox,
+  IconSvgClipboard,
+  IconSvgDatabase,
+  IconSvgFiles,
+  IconSvgHealth,
+  IconSvgLayers,
+  IconSvgMusic,
+  IconSvgTv,
+  IconSvgWifi1,
+  IconSvgWifi2,
+} from "./icons";
+
+export const DEGREES = [
+  {
+    title: "Bachelor of Engineering",
+    dateStart: "März 2012",
+    dateEnd: "August 2015",
+    paragraphs: [
+      "Ich habe an der Hochschule für Technik, Wirtschaft und Gestaltung Konstanz (HTWG-Konstanz) den Bachelor-Studiengang Elektro- und Informationstechnik mit dem Schwerpunkt Software Engineering studiert.",
+      'Thema meiner Bachelorarbeit war die "Realisierung einer günstigen Handschrift-Digitalisierung mit Hilfe von Infrarot-Technologie".',
+    ],
+    imgUrl: "./images/htwg_konstanz.jpg",
+    imgTitle:
+      "Hochschule für Technik, Wirtschaft und Gestaltung Konstanz (HTWG-Konstanz)",
+    imgAlt: "Hochschulgebäude",
+  },
+  {
+    title: "Master of Engineering",
+    dateStart: "September 2015",
+    dateEnd: "Februar 2017",
+    paragraphs: [
+      "Direkt im Anschluss studierte ich ebenfalls an der HTWG-Konstanz den Master-Studiengang Elektrische Systeme. Den Schwerpunkt habe ich dabei auf Embedded Software Engineering gelegt.",
+      'Thema der Masterarbeit war die "Erweiterung eines Technologie Demonstrators um ein Industrie 4.0 Interface".',
+    ],
+    imgUrl: "./images/htwg_konstanz.jpg",
+    imgTitle:
+      "Hochschule für Technik Wirtschaft und Gestaltung Konstanz (HTWG-Konstanz)",
+    imgAlt: "Hochschulgebäude",
+  },
+];
+
+export const WORK_EXPERIENCES = [
+  {
+    title: "Precision Motors Deutsche Minebea GmbH",
+    dateStart: "September 2016",
+    dateEnd: "Februar 2017",
+    paragraphs: [
+      "Die Precision Motors Deutsche Minebea GmbH entwickelt und produziert Bürstenlose Gleichstrommotoren (BLDCs). Sie werden unter anderem in Festplatten und im automotiven Sektor verwendet.",
+      "Als Masterand habe ich an der Erweiterung eines Technologie-Demonstrators um eine kabellose Schnittstelle gearbeitet. Nach dem Vergleich verschiedener Technologien wurde die Firmware des Technologie-Demonstrator in der Programmiersprache C um eine DECT Schnittstelle erweitert.",
+    ],
+    imgUrl: "./images/pmdm_bldc.png",
+    imgTitle: "Bürstenloser Gleichstrommotor (BLDC) der Firma PMDM",
+    imgAlt: "Bürstenloser Gleichstrommotor",
+  },
+  {
+    title: "Chromasens GmbH",
+    dateStart: "März 2017",
+    dateEnd: "November 2018",
+    paragraphs: [
+      "Die Chromasens GmbH entwickelt und produziert Zeilenkameras die hauptsächlich in OEM Produkten zu Einsatz kommen. Sie werden in Produktionen für Hochgeschwindigkeits-Qualitätskontrollen eingesetzt.",
+      "Als Embedded Software Engineer war ich verantwortlich für die Portierung der bestehenden Kamera Firmware (C-Code) auf die neu zu entwickelnde allPIXA evo Plattform. Die Firmware wurde erweitert, sodass sie mit dem GenICam Standard angesteuert werden kann.",
+    ],
+    imgUrl: "./images/chromasens_allPIXAevo.png",
+    imgTitle: "Chromasens Zeilenkamera allPIXAevo",
+    imgAlt: "Zeilenkamera",
+  },
+  {
+    title: "Bernina International AG",
+    dateStart: "Dezember 2018",
+    dateEnd: "März 2024",
+    paragraphs: [
+      "Die Bernina International AG produziert High-End Näh- und Stickmaschinen für die private und semi-professionelle Nutzung.",
+      "Als Software Engineer war ich für die Erweiterung des bereits bestehenden Bernina Software Frameworks (C++14) um Kamera und Scanner Komponenten verantwortlich die mit der Bernina B990 auf den Markt kommen werden.",
+    ],
+    imgUrl: "./images/bernina_b990.jpg",
+    imgTitle: "Bernina B990 Näh- und Stickmaschine",
+    imgAlt: "Näh- und Stickmaschine",
+  },
+];
+
+export const TRAININGS = [
+  {
+    title:
+      "Micro Consult: Embedded-Echtzeit-Linux vom Bootloader bis zum Realtime-System",
+    dateStart: "August 2018",
+    paragraphs: [
+      "Der von Micro Consult angebotene Kurs vermittelt das Wissen wie man ein Linux Image in Bezug auf Footprint, Libraries, RT-Anforderungen und Device-Tree an die eigenen Bedürfnissen anpassen kann.",
+    ],
+  },
+
+  {
+    title: "Seminar-Experts GmbH: openCV",
+    dateStart: "Januar 2019",
+    paragraphs: [
+      "Die durch Seminar-Experts GmbH durchgeführte Schulung vermittelt das Wissen wie man mit Hilfe von openCV Bildmanipulationen und Bildauswertungen durchführen kann.",
+    ],
+  },
+  {
+    title: "iSAQB CPSA Foundation Level",
+    dateStart: "Juli 2020",
+    paragraphs: [
+      "Die iSAQB CPSA Foundation Level Schulung vermittelt die Grundlagen von Software-Architekturen, Komponenten, Schnittstellen, Architekturziele, Qualitätsziele und Werkzeuge.",
+    ],
+  },
+  {
+    title: "iSAQB CPSA Advanced Level Modul DDD (Domain Driven Design)",
+    dateStart: "Oktober 2020",
+    paragraphs: [
+      "Die iSAQB CPSA DDD Schulung vermittelt das Wissen wie in Zusammenarbeit mit Stakeholdern die Domänen und Schnittstellen der zu erstellenden Software identifiziert werden können.",
+    ],
+  },
+  {
+    title:
+      "iSAQB CPSA Advanced Level Modul IMPROVE (Evolution und Verbesserung von Softwarearchitekturen)",
+    dateStart: "Februar 2021",
+    paragraphs: [
+      "Die iSAQB CPSA IMPROVE Schulung vermittelt verschiedene Strategien und Werkzeuge die dazu genutzt werden können bereits bestehende Software zu optimieren.",
+    ],
+  },
+  {
+    title:
+      "iSAQB CPSA Advanced Level Modul SOFT (Soft Skills für Softwarearchitekten)",
+    dateStart: "November 2021",
+    paragraphs: [
+      "Die iSAQB CPSA SOFT Schulung vermittelt Soft Skills die dabei Helfen die Kommunikation mit Stakeholdern konstruktiv zu gestalten. Dazu gehören unter anderem Gesprächstechniken, Fragetechniken und Konstruktives Feedback.",
+    ],
+  },
+  {
+    title:
+      "Udemy - Denis Panjuta: Lerne Flutter & Dart und erstelle native iOS & Android Apps",
+    dateStart: "Mai 2024",
+    dateEnd: "Juni 2024",
+    paragraphs: [
+      'Die Udemy Schulung "Lerne Flutter & Dart und erstelle native iOS & Android Apps" vermittelt durch mehrere Mit-Mach-Projekte das Wissen, wie iOS und Android Apps mit Flutter erstellt werden können. Dazu gehört der allgemeine Aufbau einer Flutter App, einbinden externer Libaries, API Einbindung und der Ablauf einer Veröffentlichung der erstellten App in einem Appstore.',
+    ],
+  },
+  {
+    title: "Udemy - Angela Yu: The Complete 2024 Web Development Bootcamp",
+    dateStart: "Juni 2024",
+    dateEnd: "August 2024",
+    paragraphs: [
+      'Die auf 3 Monate Lernzeit ausgelegte Udemy Schulung "The Complete 2024 Web Development Bootcamp" vermittelt durch Mit-Mach-Projekte und Theorie-Einheiten Wissen über HTML, CSS, JS, Bootstrap, EJS, React, node.js, PostgreSQL, API Nutzung, API Erstellung, Nutzer Authentifizierung (Hashing, Salting, OAuth).',
+    ],
+  },
+  {
+    title:
+      "Udemy - Jonas Schmedtmann: Master modern React from beginner to advanced",
+    dateStart: "September 2024",
+    dateEnd: "aktuell",
+    paragraphs: [
+      'Die Udemy Schulung "Master modern React from beginner to advanced" vermittelt anhand von Mit-Mach-Projekten und Theorie-Einheiten das Wissen wie React Apps aufgebaut werden können und die allgemeine Funktionsweise von React.',
+    ],
+  },
+];
+
+export const SAMPLES = [
+  {
+    title: "",
+    dateStart: "",
+    paragraphs: [],
+  },
+  {
+    title: "",
+    dateStart: "",
+    paragraphs: [],
+  },
+];
+
+export const SKILLS = [
+  {
+    title: "Enthusiastisch",
+    description: "Reinschnuppern. Begeistern. Anfangen. Fertigstellen. Freuen.",
+    icon: IconSvgBatteryCharging(),
+  },
+  {
+    title: "Selbstständig",
+    description:
+      "Anforderungen sammeln. Projekte managen. Stakeholder einbeziehen.",
+    icon: IconSvgBackback(),
+  },
+  {
+    title: "Zuverlässig",
+    description: "Testen. Dokumentieren. Abliefern.",
+    icon: IconSvgDatabase(),
+  },
+];
+
+export const TECHNOLOGIES = [
+  {
+    title: "Professionell",
+    description: "C++14. QML. Qt 5.12.",
+    icon: IconSvgWifi2(),
+  },
+  {
+    title: "Anfänger",
+    description: "Python. Flutter. React.",
+    icon: IconSvgWifi1(),
+  },
+];
+
+export const TOOLS = [
+  {
+    title: "Aufgabenverwaltung",
+    description: "Jira",
+    icon: IconSvgClipboard(),
+  },
+  {
+    title: "Dokumentation",
+    description: "Confluence",
+    icon: IconSvgFiles(),
+  },
+  {
+    title: "Versionsverwaltung",
+    description: "SVN. GitHub.",
+    icon: IconSvgLayers(),
+  },
+];
+
+export const HOBBIES = [
+  {
+    title: "Tanz",
+    description: "Salsa. Jive.",
+    icon: IconSvgMusic(),
+  },
+  {
+    title: "Musik",
+    description: "Live-Konzerte. Klavier spielen.",
+    icon: IconSvgBoomBox(),
+  },
+  {
+    title: "TV",
+    description: "Monk. Psych. Castle.",
+    icon: IconSvgTv(),
+  },
+  {
+    title: "Lesen",
+    description: "Fantasy. Abenteuer. Krimis.",
+    icon: IconSvgBook(),
+  },
+  {
+    title: "Gesundheit",
+    description: "Spaziergänge. Velo. Yoga. Kochen.",
+    icon: IconSvgHealth(),
+  },
+];
+
+export const SHOP_ITEMS = [
+  {
+    imgUrl: "./images/shop_login.png",
+    title: "Log-In Seite",
+    alt: "Log-In Seite",
+  },
+  {
+    imgUrl: "./images/shop_landing_page.png",
+    title: "Hauptseite",
+    alt: "Hauptseite",
+  },
+  {
+    imgUrl: "./images/shop_menu.png",
+    title: "Menü Leiste",
+    alt: "Menü Leiste",
+  },
+  {
+    imgUrl: "./images/shop_own_products.png",
+    title: "Eigene Waren Seite",
+    alt: "Eigene Waren Seite",
+  },
+  {
+    imgUrl: "./images/shop_cart.png",
+    title: "Einkaufswagen",
+    alt: "Einkaufswagen",
+  },
+];
+
+export const PMT_ITEMS = [
+  {
+    imgUrl: "./images/pmt_active_sprints.png",
+    title: "Aktive Sprints",
+    alt: "Aktive Sprints",
+  },
+  {
+    imgUrl: "./images/pmt_sprint_planner.png",
+    title: "Sprint Planer",
+    alt: "Sprint Planer",
+  },
+  {
+    imgUrl: "./images/pmt_database_browser.png",
+    title: "Aufgabenverwaltung",
+    alt: "Aufgabenverwaltung",
+  },
+  {
+    imgUrl: "./images/pmt_timeline.png",
+    title: "Projektplanung",
+    alt: "Projektplanung",
+  },
+];
